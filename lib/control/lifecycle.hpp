68501d0464d03a939a422aedb5b925cbcdb40801
David Alexander <dmalexa5@ncsu.edu>
2026-03-09 09:26:36 -0400

    Lifecycle WIP

---
+++ lib/control/lifecycle.hpp
@@ -0,0 +1,94 @@
+#pragma once
+
+#define SERIAL_BAUD_RATE 115200
+
+enum class Status
+{
+  ERROR,
+  ACTIVE,
+  COMPLETE
+};
+using StatusCallback = Status (*)();
+
+enum class Error
+{
+  OK,
+  INVALID_SERIAL,
+  INVALID_SETPOINT,
+  CONTROL_TIMEOUT,
+  INVALID_TRANSITION
+};
+
+
+struct Setpoint
+{
+  Setpoint(
+    float x,
+    float y,
+    float tolerance,
+    float velocity,
+    unsigned long int timeout
+  ) : x(x), y(y), tolerance(tolerance), velocity(velocity), timeout(timeout) {};
+
+  float x;
+  float y;
+  float tolerance;
+  float velocity;
+  int timeout;
+};
+
+class State
+{
+  public:
+    State() {};
+
+    /** @brief The currently active control loop callback */
+    StatusCallback callback = []() { return Status::ERROR; };
+
+    /** @brief The currently active system setpoint */
+    Setpoint* setpoint;
+
+    /** @brief The currently active error, if any */
+    Error* error;
+
+    /** @brief Whether or not a response is due to the serial interface */
+    bool response_due = false;
+
+    inline void reset(StatusCallback callback) {
+      setpoint = nullptr;
+      error = nullptr;
+      this->callback = callback;
+    }
+
+};
+
+const char* processError(Error error)
+{
+  switch (error)
+  {
+    case Error::INVALID_SERIAL:
+      return "ERROR Invalid serial message received.";
+    case Error::INVALID_SETPOINT:
+      return "ERROR Invalid setpoint received.";
+    case Error::CONTROL_TIMEOUT:
+      return "ERROR Control timed out. Collision likely!";
+    case Error::INVALID_TRANSITION:
+      return "ERROR Invalid transition. Recalibrate.";
+    default:
+      return "ERROR Unknown error occurred.";
+  }
+}
+
+const char* processStatus(Status status) {
+  switch (status)
+  {
+    case Status::ACTIVE:
+      return "OK ACTIVE";
+    case Status::COMPLETE:
+      return "OK COMPLETE";
+    case Status::ERROR:
+      return "ERROR Received status in error state";
+    default:
+      return "ERROR Unknown status";
+  }
+}
--- src/main.cpp
+++ src/main.cpp
@@ -3,13 +3,7 @@
 
 #include "lifecycle.hpp"
 
-// Lifecycle variables
-Status status = Status::INACTIVE;
-Transition transition = Transition::NONE;
-bool response_due = false;
-static char serial_buffer[64]; // IO buffer
-Setpoint setpoint; // Actively tracked setpoint
-StatusCallback callback; // Active controller callback
+State state;
 
 #if defined(ARDUINO)
 
@@ -17,18 +11,89 @@ StatusCallback callback; // Active controller callback
 
 Status activeControl() {
   delay(500);
-  return Status::FINISHED;
+  return Status::COMPLETE;
 }
 
 Status calibrateControl() {
   delay(500);
-  setpoint = home;
-  return Status::CALIBRATED;
+  return Status::COMPLETE;
 }
 
 Status inactiveControl() {
   delay(100);
-  return Status::INACTIVE;
+  return Status::COMPLETE;
+}
+
+/**
+ * @brief Parse an incoming serial message
+ * @param message The incoming char buffer
+ */
+Error parseSerial(const char* message)
+{
+
+  if (strncmp(message, "ACTIVATE", 8) == 0) {
+    state.reset(calibrateControl);
+    return Error::OK;
+  }
+
+  if (strncmp(message, "DEACTIVATE", 10) == 0) {
+    state.reset(inactiveControl);
+    return Error::OK;
+  }
+
+  if (strncmp(message, "SETPOINT", 8) == 0) {
+
+    if (state.callback == inactiveControl 
+     || state.callback == calibrateControl ) { return Error::INVALID_TRANSITION; }
+
+    float x, y, tolerance, velocity;
+    int timeout;
+    if (sscanf(message, "SETPOINT %f %f %f %f %d", 
+      &x, &x, &tolerance, &velocity, &timeout) == 2) {
+        state.setpoint = &Setpoint(x, y, tolerance, velocity, timeout);
+        return Error::OK;
+    } else {
+      return Error::INVALID_SETPOINT;
+    }
+  }
+
+  return Error::INVALID_SERIAL;
+}
+
+
+/**
+ * @brief Get the serial buffer, assuming one is available
+ */
+void getSerial()
+{
+  static char serial_buffer[64];
+  serial_buffer[0] = '\0';
+  int index = 0;
+
+  while (Serial.available() > 0) {
+    char incoming = Serial.read();
+
+    if (incoming == '\n') { // Message is complete
+        serial_buffer[index] = '\0'; // Null-terminate the string
+        parseSerial(serial_buffer); 
+        index = 0; // Reset for next message
+    } 
+    else if (index < 63) { // Avoid buffer overflow
+        serial_buffer[index++] = incoming;
+    }
+  }
+}
+
+/**
+ * @brief Respond with the appropriate serial message
+ */
+void respondSerial(Status status) {
+  if (nullptr == state.error || *state.error == Error::OK)
+  {
+    Serial.println(processStatus(status));
+  } else {
+    Serial.println(processError(*state.error));
+  }
 }
 
 void setup()
@@ -36,29 +101,22 @@ void setup()
   Serial.begin(SERIAL_BAUD_RATE);
   while(!Serial);
 
-  callback = inactiveControl;
-
 }
 
 void loop()
 {
-  
-  readSerial();
-
-  switch (transition)
-  {
-  case Transition::NONE:
-    break;
-  case Transition::ACTIVATE:
-    callback = calibrateControl;
-    break;
-  case Transition::DEACTIVATE:
-    callback = inactiveControl;
+  if (Serial.available()) {
+    getSerial();
+    state.response_due = true;
   }
 
-  status = callback();
+  auto status = state.callback();
+    
 
-  if(response_due) { respondSerial(); }
+  if(state.response_due) {
+    respondSerial(status);
+    state.response_due = false;
+  }
 
 }
 
